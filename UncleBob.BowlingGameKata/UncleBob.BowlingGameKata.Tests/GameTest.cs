2c24f046d0ba1c5b49b264616a3c69012e79557d
Stew Fidgeon <stewfidgeon@gmail.com>
2018-08-21 13:48:34 +0100

    SF: Third Test

--- UncleBob.BowlingGameKata/UncleBob.BowlingGameKata.Tests/GameTest.cs
+++ UncleBob.BowlingGameKata/UncleBob.BowlingGameKata.Tests/GameTest.cs
@@ -38,6 +38,16 @@ namespace UncleBob.BowlingGameKata.Tests
             Assert.AreEqual(20, g.Score());
         }
 
+        //[TestMethod]
+        //public void TestOneSpare()
+        //{
+        //    g.Roll(5);
+        //    g.Roll(5);
+        //    g.Roll(3);
+        //    RollMany(17, 0);
+        //    Assert.AreEqual(16, g.Score());
+        //}
+
         private void RollMany(int n, int pins)
         {
             for (var i = 0; i < n; i++)
