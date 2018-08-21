556c03285371407bca8f62a54a47ffce61ab6498
Stew Fidgeon <stewfidgeon@gmail.com>
2018-08-21 13:09:20 +0100

    SF: First Test

---
+++ UncleBob.BowlingGameKata/UncleBob.BowlingGameKata.Tests/GameTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UncleBob.BowlingGameKata.Tests
+{
+    [TestClass]
+    public class GameTest
+    {
+        [TestMethod]
+        public void TestGutterGame()
+        {
+            var g = new Game();
+        }
+    }
+}
---
+++ UncleBob.BowlingGameKata/UncleBob.BowlingGameKata/Game.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UncleBob.BowlingGameKata
+{
+    public class Game
+    {
+    }
+}
