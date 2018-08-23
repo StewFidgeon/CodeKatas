77a569613d19db998130876a608c725f0f339b6b
Stew Fidgeon <stewfidgeon@gmail.com>
2018-08-23 16:25:16 +0100

    SF: Prime Factors Kata - Test One

---
+++ UncleBob.PrimeFactorsKata/UncleBob.PrimeFactorsKata.Tests/PrimeFactorsTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UncleBob.PrimeFactorsKata.Tests
+{
+    [TestClass]
+    public class PrimeFactorsTest
+    {
+        private List<int> _list = new List<int>();
+
+        [TestMethod]
+        public void TestOne()
+        {
+            CollectionAssert.AreEqual(_list, PrimeFactors.Generate(1));
+        }
+    }
+}
---
+++ UncleBob.PrimeFactorsKata/UncleBob.PrimeFactorsKata/PrimeFactors.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UncleBob.PrimeFactorsKata
+{
+    public class PrimeFactors
+    {
+        public static List<int> Generate(int n)
+        {
+            return new List<int>();
+        }
+    }
+}
