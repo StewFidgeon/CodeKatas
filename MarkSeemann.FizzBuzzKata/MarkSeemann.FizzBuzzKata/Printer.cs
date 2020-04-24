72926fcaf760fcf8f4143ebc408d907731b2f3ab
Stew Fidgeon <stewfidgeon@gmail.com>
2020-04-24 11:13:07 +0100

    FizzBuzz green

--- MarkSeemann.FizzBuzzKata/MarkSeemann.FizzBuzzKata/Printer.cs
+++ MarkSeemann.FizzBuzzKata/MarkSeemann.FizzBuzzKata/Printer.cs
@@ -10,7 +10,7 @@ namespace MarkSeemann.FizzBuzzKata
         {
             var result = string.Empty;
             for (var i = 1; i <= 100; i++)
-                result = string.Format("{0}{1},", result, i);
+                result = string.Format("{0}{1},", result, i % 3 == 0 ? "Fizz" : i.ToString());
             return result.Substring(0, result.Length -1);
         }
     }
