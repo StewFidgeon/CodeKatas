5b010447f7d9e4a34d27e01ae9157ddb6415367b
Stew Fidgeon <stewfidgeon@gmail.com>
2020-04-24 11:02:10 +0100

    Added Kata Mark Seeman Fizz Buzz, with first failing test

---
+++ MarkSeemann.FizzBuzzKata/MarkSeemann.FizzBuzzKata/Printer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarkSeemann.FizzBuzzKata
+{
+    public class Printer
+    {
+        public string GetOutput()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
