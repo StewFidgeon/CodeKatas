0fe7d5c8389c944199ad06d10628b8ade5e76ab6
Stew Fidgeon <stewfidgeon@gmail.com>
2020-05-15 13:27:59 +0100

    Got password features under test

--- MarkSeemann.LegacyCodeKata/MarkSeemann.LegacyCodeKata.Tests/SecurityManagerTests.cs
+++ MarkSeemann.LegacyCodeKata/MarkSeemann.LegacyCodeKata.Tests/SecurityManagerTests.cs
@@ -8,7 +8,7 @@ namespace MarkSeemann.LegacyCodeKata.Tests
     public class SecurityManagerTests
     {
         [Fact]
-        public void GivenAnInputSource_UserIsCreated()
+        public void GivenAValidInput_UserIsCreated()
         {
             var source = new Mock<IInputSource>();
             source.Setup(s => s.GetInput());
@@ -24,6 +24,51 @@ namespace MarkSeemann.LegacyCodeKata.Tests
             var sm = new SecurityManager(source.Object, spy);
             sm.CreateUser();
 
+            DoAssertions(spy, expected);
+        }
+
+        [Fact]
+        public void GivenAnUnconfirmedPasswordInput_UserIsNotCreated()
+        {
+            var source = new Mock<IInputSource>();
+            source.Setup(s => s.GetInput());
+            source.Setup(s => s.Username).Returns("username");
+            source.Setup(s => s.Fullname).Returns("user name");
+            source.Setup(s => s.Password).Returns("password");
+            source.Setup(s => s.PasswordConfirmation).Returns("wrongpassword");
+
+            var spy = new SpyOutputDestination();
+
+            var expected = new List<string> { "The passwords don't match" };
+
+            var sm = new SecurityManager(source.Object, spy);
+            sm.CreateUser();
+
+            DoAssertions(spy, expected);
+        }
+
+        [Fact]
+        public void GivenAnInvalidPasswordInput_UserIsNotCreated()
+        {
+            var source = new Mock<IInputSource>();
+            source.Setup(s => s.GetInput());
+            source.Setup(s => s.Username).Returns("username");
+            source.Setup(s => s.Fullname).Returns("user name");
+            source.Setup(s => s.Password).Returns("pass");
+            source.Setup(s => s.PasswordConfirmation).Returns("pass");
+
+            var spy = new SpyOutputDestination();
+
+            var expected = new List<string> { "Password must be at least 8 characters in length" };
+
+            var sm = new SecurityManager(source.Object, spy);
+            sm.CreateUser();
+
+            DoAssertions(spy, expected);
+        }
+
+        private void DoAssertions(SpyOutputDestination spy, List<string> expected)
+        {
             Assert.Equal(expected.Count, spy.Outputs.Count);
             for (int i = 0; i < expected.Count; i++)
                 Assert.Equal(expected[i], spy.Outputs[i]);
